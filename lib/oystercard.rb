07161a8327ca673cfdea0470df6c9a1807b23e3e
Edward Phillips <edd0ddi0@gmail.com>
2020-03-23 16:39:51 +0000

    added top-up tests and method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,39 @@
 class Oystercard
-  def balance
-    return 10
+
+  attr_reader :balance
+
+  STARTING_BALANCE = 0
+
+  def initialize(balance = STARTING_BALANCE)
+    @balance = balance
   end
+
+  def top_up(num)
+    @balance += num
+  end
+  
 end
+
+
+
+=begin
+reader
+def balance
+  @balance
+end
+
+
+writer
+
+def balance(num)
+  @balance = num
+end
+
+accessor
+def balance(num)
+  @balance = num
+end
+def balance
+  @balance
+end
+=end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -9,6 +9,15 @@ describe Oystercard do
     it "balance should respond to class with an integer" do
       expect(card.balance.class).to eq(Integer)
     end
+    it "should respond to top_up" do
+      expect(card).to respond_to(:top_up)
+    end
+    it "balance should be increased by topping up" do
+      new_card = Oystercard.new(0)
+      y = new_card.balance
+      x = 10
+      expect { new_card.top_up(x) }.to change { new_card.balance }.from(y).to(x+y)
+    end
   end
 end
 
