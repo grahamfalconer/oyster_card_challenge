5050b6dde0bf015b1a7608be259d58c89eb38a5c
Graham <grahamalconer@icloud.com>
2020-03-23 17:46:06 +0000

    CardcanDeduct,tested

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,16 +3,24 @@ class Oystercard
   attr_reader :balance
 
   STARTING_BALANCE = 0
+  CARD_LIMIT = 90
 
   def initialize(balance = STARTING_BALANCE)
     @balance = balance
   end
 
   def top_up(num)
+    raise "you cannot top up #{num} as it brings your card over the limit" if @balance + num > CARD_LIMIT
     @balance += num
   end
-  
+
+  def deduct
+    @balance -= 1
+  end
+
 end
+
+
 
 
 
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,7 +1,8 @@
 require './lib/oystercard'
 
 describe Oystercard do
-  let(:card) {Oystercard.new}
+  let(:card) { Oystercard.new }
+  let(:new_card) { Oystercard.new(50) }
   describe 'balance' do
     it "should respond to balance" do
       expect(card).to respond_to(:balance)
@@ -13,13 +14,25 @@ describe Oystercard do
       expect(card).to respond_to(:top_up)
     end
     it "balance should be increased by topping up" do
-      new_card = Oystercard.new(0)
       y = new_card.balance
       x = 10
       expect { new_card.top_up(x) }.to change { new_card.balance }.from(y).to(x+y)
     end
+    it "doesn't top up past limit" do
+      card_limit = 90
+      expect { card.top_up(card_limit + 1)  }.to raise_error "you cannot top up #{card_limit + 1} as it brings your card over the limit"
+    end
+    it "should respond to deduct" do
+      expect(card).to respond_to(:deduct)
+    end
+    it "balance should be decreased by deduct" do
+      y = new_card.balance
+      new_card.deduct
+      expect(new_card.balance).to be < (y)
+    end
   end
 end
+
 
 
 #my_num = 5
